ca79f7e19b255f7b00d1d3b3f07c1d0061945739
Baptiste Desbiolles <bapt.desbiolles@gmail.com>
2013-07-12 23:00:18 +0200

    read from index.html 2

--- web.js
+++ web.js
@@ -3,10 +3,11 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 var file = fs.readFile('index.html');
+var buf = new Buffer(file);
 
 app.get('/', function (request, response) {
 	"use strict";
-    response.send(file);
+    response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
