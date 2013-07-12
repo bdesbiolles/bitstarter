1aa9dca869a126fb6fd4733f2ac6be9c38ff5f7a
Baptiste Desbiolles <bapt.desbiolles@gmail.com>
2013-07-12 22:39:41 +0200

    Updated web.js

--- web.js
+++ web.js
@@ -2,11 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function (request, response) {
+	"use strict";
+    response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port, function () {
+	"use strict";
+    console.log("Listening on " + port);
 });
